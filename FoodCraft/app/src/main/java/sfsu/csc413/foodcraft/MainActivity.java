62a7460bad827e036b543251a5fb242b47afbf0d
Paul Klein <paulleoklein@gmail.com>
2015-10-13 12:50:11 -0700

    Working build of Barcode scanning via Intents using ZXing.

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/MainActivity.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/MainActivity.java
@@ -2,15 +2,92 @@ package sfsu.csc413.foodcraft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import android.content.Intent;
+
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button upcbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.upcbutton = (Button) this.findViewById(R.id.upc);
+        this.upcbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            fetch_ingrident_via_UPC(scanResult.getContents());
+        }
+
+    }
+
+    public void fetch_ingrident_via_UPC(String upc_code) {
+        String url = "https://api.outpan.com/v1/products/" + upc_code + "/name";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (com.android.volley.Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (!response.getString("name").equals("null")) {
+                                send_toast(response.getString("name"));
+                            } else {
+                                send_toast("UPC Not Found!");
+                            }
+                        } catch (Exception exception) {
+                            send_toast(exception.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> params = new HashMap<String, String>();
+                String creds = String.format("%s:", "459563971cd36022e52e0c936ce2836c");
+                String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+                params.put("Authorization", auth);
+                return params;
+            }
+        };
+        VolleyRequest.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
+
+    public void send_toast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -19,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
