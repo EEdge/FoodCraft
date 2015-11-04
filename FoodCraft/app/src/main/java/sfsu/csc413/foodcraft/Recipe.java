64206103cb0db58fc3af026c3a0b5071f8353034
Brook Thomas <brookthomas@gmail.com>
2015-11-04 11:32:25 -0800

    added ingredient list to Recipe class

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/Recipe.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/Recipe.java
@@ -1,5 +1,9 @@
 package sfsu.csc413.foodcraft;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
@@ -7,10 +11,12 @@ public class Recipe {
 
     protected int api;
     protected String id;
+    protected List<String> ingredients;
 
     Recipe(int api, String id) {
         this.api = api;
         this.id = id;
+        this.ingredients = new ArrayList<>();
     }
 
 }
