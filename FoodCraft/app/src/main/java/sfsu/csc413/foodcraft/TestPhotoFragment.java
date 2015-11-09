1fdb1f5e35e25e92071c41fe898f0152e20229b3
Evan Edge <evanjedge@gmail.com>
2015-11-09 01:38:18 -0800

    Work done on UPC frag. scanner - still not working

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/TestPhotoFragment.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/TestPhotoFragment.java
@@ -1,8 +1,11 @@
 package sfsu.csc413.foodcraft;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,18 +14,47 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.zxing.ResultPoint;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CameraPreview;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
 import com.journeyapps.barcodescanner.camera.CameraInstance;
+
+import java.util.List;
+
+import static sfsu.csc413.foodcraft.R.layout.fragment_test_photo;
 
 
 public class TestPhotoFragment extends Fragment {
 
     private String toast;
+    private CompoundBarcodeView barcodeView;
+
+
+
+        BarcodeCallback callback = new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                if (result.getText() != null) {
+                    barcodeView.setStatusText(result.getText());
+                }
+                //Added preview of scanned barcode
+                ImageView imageView = (ImageView) getView().findViewById(R.id.barcode_scanner_image);
+                imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+            }
+        };
+
+
 
     public TestPhotoFragment() {
     }
@@ -30,22 +62,24 @@ public class TestPhotoFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        View view = getView();
         displayToast();
+        scanFromFragment(view);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_test_photo, container, false);
-        scanFromFragment();
+        View view;
+        view = (View) inflater.inflate(fragment_test_photo, container, false);
         return view;
     }
 
-    public void scanFromFragment() {
-//        IntentIntegrator integrator = new IntentIntegrator(this.getActivity());
-//        integrator.setCaptureActivity(UPCScan.class);
-//        integrator.initiateScan();
-        IntentIntegrator.forFragment(this).initiateScan();
+    @TargetApi(Build.VERSION_CODES.M)
+    public void scanFromFragment(View view) {
+        barcodeView = (CompoundBarcodeView) view.findViewById(R.id.barcode_scanner);
+        barcodeView.decodeContinuous(callback);
+
     }
 
     private void displayToast() {
