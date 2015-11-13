8ea6fe7cf17eaf97113ac873740074405759ba89
Evan Edge <evanjedge@gmail.com>
2015-11-13 12:55:11 -0800

    Fixed ingredient activity search button

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/SearchableIngredientFragment.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/SearchableIngredientFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -79,32 +80,139 @@ public class SearchableIngredientFragment extends Fragment implements AbsListVie
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        populateArray();
-//        ArrayAdapter lvIngredientSearchAdapter = new ArrayAdapter<>(getActivity(), list_item_searchable_ingredients, searchable_ingredient_item, searchableIngredients);
-//        lvIngredientSearch = (ListView) getView().findViewById(R.id.listView1);
-//        lvIngredientSearch.setAdapter(lvIngredientSearchAdapter);
-//        lvIngredientSearch.setTextFilterEnabled(true);
-//
-//        //on click of searchable list item, add item to selected list
-//        lvIngredientSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapter, View v, int position,
-//                                    long arg3) {
-//                String value = (String) adapter.getItemAtPosition(position);
-//                if (!isInArray(value, IngredientSearch.selectedFoods)) {
-//                    IngredientSearch.selectedFoods.add(0, value);
-//                    IngredientSearch.lvSelectedIngredients.setAdapter(IngredientSearch.lvSelectedIngredientsAdapter);
-//                }
-//            }
-//        });
-//
-//        //initialize SearchView for searchable ingredient list
-//        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView = (SearchView) getActivity().findViewById(R.id.menu_item_search);
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
-//        searchView.setSubmitButtonEnabled(true);
-//        searchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);//listen for text change
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
+
+        populateArray();
+        final ArrayAdapter lvIngredientSearchAdapter = new ArrayAdapter<>(getActivity(), list_item_searchable_ingredients, searchable_ingredient_item, searchableIngredients);
+        lvIngredientSearch = (ListView) view.findViewById(R.id.listView1);
+        lvIngredientSearch.setAdapter(lvIngredientSearchAdapter);
+        lvIngredientSearch.setTextFilterEnabled(true);
+
+        //on click of searchable list item, add item to selected list
+        lvIngredientSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3) {
+                String value = (String) adapter.getItemAtPosition(position);
+                if (!isInArray(value, IngredientSearch.selectedFoods)) {
+                    IngredientSearch.selectedFoods.add(0, value);
+                    IngredientSearch.lvSelectedIngredients.setAdapter(IngredientSearch.lvSelectedIngredientsAdapter);
+                    searchableIngredients.remove(position);
+                    lvIngredientSearchAdapter.notifyDataSetChanged();
+
+                }
+            }
+        });
+
+        IngredientSearch.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //on text submit, filter list to show search results as user types
+                filter(searchableIngredients, query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //on text change, filter list to show search results as user types
+                filter(searchableIngredients, newText);
+                return true;
+            }
+        });//listen for text change
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private boolean isInArray(String string, ArrayList<String> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (string == arrayList.get(i))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (null != mListener) {
+            // Notify the active callbacks interface (the activity, if the
+            // fragment is attached to one) that an item has been selected.
+            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
+        }
+    }
+
+    /**
+     * The default content for this Fragment has a TextView that is shown when
+     * the list is empty. If you would like to change the text, call this method
+     * to supply the text it should use.
+     */
+    public void setEmptyText(CharSequence emptyText) {
+        View emptyView = lvIngredientSearch.getEmptyView();
+
+        if (emptyView instanceof TextView) {
+            ((TextView) emptyView).setText(emptyText);
+        }
+    }
+
+    private void filter(ArrayList<String> searchableListArray, CharSequence searchQuery) {
+        //filter results by matching the query string and setting a new array to the ArrayAdapter on each text change
+        searchQuery = searchQuery.toString().toLowerCase();
+
+        ArrayList<String> newFilterResults;
+
+        if (searchQuery != null && searchQuery.length() > 0) {
+
+
+            ArrayList<String> auxData = new ArrayList<>();
+
+            for (int i = 0; i < searchableListArray.size(); i++) {
+                if (searchableListArray.get(i).toLowerCase().contains(searchQuery))
+                    auxData.add(searchableListArray.get(i));
+            }
+
+            newFilterResults = auxData;
+        } else {
+
+            newFilterResults = searchableListArray;
+        }
+        lvIngredientSearchAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_searchable_ingredients, R.id.searchable_ingredient_item, newFilterResults);
+        lvIngredientSearch.setAdapter(lvIngredientSearchAdapter);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(String id);
     }
 
     private void populateArray () {
@@ -616,136 +724,6 @@ public class SearchableIngredientFragment extends Fragment implements AbsListVie
         searchableIngredients.add("fagottini");
         searchableIngredients.add("ravioli");
         searchableIngredients.add("tortellini");
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
-
-        populateArray();
-        ArrayAdapter lvIngredientSearchAdapter = new ArrayAdapter<>(getActivity(), list_item_searchable_ingredients, searchable_ingredient_item, searchableIngredients);
-        lvIngredientSearch = (ListView) view.findViewById(R.id.listView1);
-        lvIngredientSearch.setAdapter(lvIngredientSearchAdapter);
-        lvIngredientSearch.setTextFilterEnabled(true);
-
-        //on click of searchable list item, add item to selected list
-        lvIngredientSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View v, int position,
-                                    long arg3) {
-                String value = (String) adapter.getItemAtPosition(position);
-                if (!isInArray(value, IngredientSearch.selectedFoods)) {
-                    IngredientSearch.selectedFoods.add(0, value);
-                    IngredientSearch.lvSelectedIngredients.setAdapter(IngredientSearch.lvSelectedIngredientsAdapter);
-                }
-            }
-        });
-
-        IngredientSearch.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //on text submit, filter list to show search results as user types
-                filter(searchableIngredients, query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //on text change, filter list to show search results as user types
-                filter(searchableIngredients, newText);
-                return true;
-            }
-        });//listen for text change
-
-        return view;
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    private boolean isInArray(String string, ArrayList<String> arrayList) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (string == arrayList.get(i))
-                return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (null != mListener) {
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
-        }
-    }
-
-    /**
-     * The default content for this Fragment has a TextView that is shown when
-     * the list is empty. If you would like to change the text, call this method
-     * to supply the text it should use.
-     */
-    public void setEmptyText(CharSequence emptyText) {
-        View emptyView = lvIngredientSearch.getEmptyView();
-
-        if (emptyView instanceof TextView) {
-            ((TextView) emptyView).setText(emptyText);
-        }
-    }
-
-    private void filter(ArrayList<String> searchableListArray, CharSequence searchQuery) {
-        //filter results by matching the query string and setting a new array to the ArrayAdapter on each text change
-        searchQuery = searchQuery.toString().toLowerCase();
-
-        ArrayList<String> newFilterResults;
-
-        if (searchQuery != null && searchQuery.length() > 0) {
-
-
-            ArrayList<String> auxData = new ArrayList<>();
-
-            for (int i = 0; i < searchableListArray.size(); i++) {
-                if (searchableListArray.get(i).toLowerCase().contains(searchQuery))
-                    auxData.add(searchableListArray.get(i));
-            }
-
-            newFilterResults = auxData;
-        } else {
-
-            newFilterResults = searchableListArray;
-        }
-        lvIngredientSearchAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_searchable_ingredients, R.id.searchable_ingredient_item, newFilterResults);
-        lvIngredientSearch.setAdapter(lvIngredientSearchAdapter);
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(String id);
     }
 
 }
