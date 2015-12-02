dbcb22897d9febe75a6340d93e78c9829f393b8a
Paul Klein <paulleoklein@gmail.com>
2015-12-01 21:28:45 -0800

    Basically fixing all the mess from my bad merging earlier. All screens working. Hard to force push to fix the bad merge.

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/RecipeDetailActivity.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/RecipeDetailActivity.java
@@ -2,20 +2,29 @@ package sfsu.csc413.foodcraft;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 
 import java.lang.String;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
     public static final String RECIPE_DETAILS = "sfsu.csc413.foodcraft.RECIPE_DETAILS";
-    TextView txt_ingredientsList, txt_servingSize, txt_totalTime, txt_title;
+    TextView txt_ingredientsList, txt_servingSize, txt_totalTime, txt_title, txt_nutritionKey,txt_nutritionValue;
+    NetworkImageView recipe_image;
 
     private RecipeDetail mRecipeDetail;
     private ArrayList<String> preferencesIngredients;
@@ -26,10 +35,12 @@ public class RecipeDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
 
-        txt_ingredientsList = (TextView)findViewById(R.id.ingredient_list);
-        txt_servingSize = (TextView)findViewById(R.id.serving_size);
-        txt_totalTime = (TextView)findViewById(R.id.total_time);
-        txt_title = (TextView)findViewById(R.id.recipe_name);
+        txt_ingredientsList = (TextView) findViewById(R.id.ingredient_list);
+        txt_servingSize = (TextView) findViewById(R.id.serving_size);
+        txt_totalTime = (TextView) findViewById(R.id.total_time);
+        txt_title = (TextView) findViewById(R.id.recipe_name);
+        txt_nutritionKey = (TextView) findViewById(R.id.nutrition_key);
+        txt_nutritionValue = (TextView) findViewById(R.id.nutrition_value);
         //preferencesIngredients = SharedPreferences.getSharedPreferences();
 
         /* Unpack */
@@ -38,18 +49,41 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
         mRecipeDetail = (RecipeDetail) bundle.getSerializable(RECIPE_DETAILS);
 
-        txt_servingSize.setText(Integer.toString(5667));
+        recipe_image = (NetworkImageView) findViewById(R.id.recipe_image);
+        ImageLoader mImageLoader = VolleyRequest.getInstance(this).getImageLoader();
+        recipe_image.setImageUrl(mRecipeDetail.imageURL, mImageLoader);
+
+        txt_title.setText(mRecipeDetail.title);
+
+        txt_servingSize.setText(Integer.toString(mRecipeDetail.numberServings));
 
         txt_totalTime.setText(mRecipeDetail.totalTime);
 
+        StringBuilder builder1 = new StringBuilder();
+        StringBuilder builder2 = new StringBuilder();
 
-        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> entry : mRecipeDetail.nutrition.entrySet()) {
+            String key = entry.getKey();
+            key = Character.toString(key.charAt(0)).toUpperCase()+key.substring(1);
+            String value = entry.getValue();
+            value = Character.toString(value.charAt(0)).toUpperCase()+value.substring(1);
+            builder1.append(key + "\n");
+            builder2.append(value + "\n");
+        }
+        txt_nutritionKey.setText(builder1.toString());
+        txt_nutritionValue.setText(builder2.toString());
+
+        //Parse image URL
+        Uri myUri = Uri.parse(mRecipeDetail.imageURL);
+        recipe_image.setImageURI(myUri);
+        Log.i("IMAGE_DETAIL", mRecipeDetail.imageURL);
+
+        StringBuilder builder3 = new StringBuilder();
         for (String details : mRecipeDetail.ingredients) {
-            builder.append(details + "\n");
+            builder3.append(details + "\n");
         }
 
-        txt_ingredientsList.setText(builder.toString());
-        }
-
+        txt_ingredientsList.setText(builder3.toString());
+    }
 
 }
