93b24133a0977be93a30af60ba686de23dd67575
Brook Thomas <brookthomas@gmail.com>
2015-12-08 22:28:52 -0800

    Utilities.java comments and formatting

--- FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/Utilities.java
+++ FoodCraft/app/src/main/java/sfsu/csc413/foodcraft/Utilities.java
@@ -6,14 +6,16 @@ import java.util.Random;
 
 /**
  * @file:Utilities.java
- * @author: brook thomas
- * @version: 0.2
- *
+ * @author: Brook Thomas
+ * @version: 1.0
  */
 public class Utilities {
 
-    /** Private constructor - class exists solely for static methods */
-    private Utilities() {}
+    /**
+     * Private constructor - class exists solely for static methods
+     */
+    private Utilities() {
+    }
 
     /**
      * Strips non-ASCII characters from a string.
@@ -21,13 +23,13 @@ public class Utilities {
      * @param ingredient An ingredient.
      * @return An ingredient stripped of non-ASCII characters.
      */
-    private static String stripUnicode (String ingredient) {
+    private static String stripUnicode(String ingredient) {
 
         StringBuffer buildString = new StringBuffer();
 
         char[] asArray = ingredient.toCharArray();
 
-        for (char x: asArray) {
+        for (char x : asArray) {
             if ((int) x <= 127) {
                 buildString.append(x);
             }
@@ -38,27 +40,27 @@ public class Utilities {
     }
 
     /**
-     *  Given a string, converts most common cases of plural to singular.
-     *  If the string was already singular it will be returned unmodified.
+     * Given a string, converts most common cases of plural to singular.
+     * If the string was already singular it will be returned unmodified.
      *
      * @param ingredient An ingredient.
      * @return An ingredient reduced from plural to singular.
      */
-    private static String removePlurals (String ingredient) {
+    private static String removePlurals(String ingredient) {
 
         // berries -> berry
         if (ingredient.length() >= 9 && ingredient.substring(ingredient.length() - 7).equals("berries")) {
-            return ingredient.substring(0,ingredient.length()-3) + "y";
+            return ingredient.substring(0, ingredient.length() - 3) + "y";
         }
 
         // es -> x
-        if (ingredient.substring(ingredient.length()-2).equals("es")) {
-            return ingredient.substring(0,ingredient.length()-2);
+        if (ingredient.substring(ingredient.length() - 2).equals("es")) {
+            return ingredient.substring(0, ingredient.length() - 2);
         }
 
         // s -> x
-        if (ingredient.substring(ingredient.length()-1).equals("s")) {
-            return ingredient.substring(0,ingredient.length()-1);
+        if (ingredient.substring(ingredient.length() - 1).equals("s")) {
+            return ingredient.substring(0, ingredient.length() - 1);
         }
 
         // base case
@@ -73,34 +75,32 @@ public class Utilities {
      * the result.
      *
      * @param ingredient An ingredient.
-     *
      * @return An ingredient stripped of unnecessary keywords.
-     *
      */
-    private static String scrubKeywords (String ingredient) {
+    private static String scrubKeywords(String ingredient) {
 
         String[] keywords = {"granulated", "boiling", "softened", "diced", "sliced", "slices", "slice",
                 "ground", "shredded", "grated", "minced", "dried", "finely chopped", "chopped",
                 "mashed", "crushed", "boiled", "baked", "cold", "warm", "hot", "warmed", "fresh", "organic",
-                "cooked", "large", "medium", "small", "plain", "distilled", "frozen", "pitted","peeled",
-                "cup", "cups", "tsp", "tbsp", "teaspoon", "tablespoon", "oz", "and", "/", ",","(",")",
-                "1","2","3","4","5","6","7","8","9","0"};
+                "cooked", "large", "medium", "small", "plain", "distilled", "frozen", "pitted", "peeled",
+                "cup", "cups", "tsp", "tbsp", "teaspoon", "tablespoon", "oz", "and", "/", ",", "(", ")",
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
 
 
         // Replace keywords with nothing
-        for (String k: keywords) {
-            ingredient = ingredient.replace(k,"");
+        for (String k : keywords) {
+            ingredient = ingredient.replace(k, "");
         }
 
-        if (ingredient.substring(0,1).equals(" ")) {
+        if (ingredient.substring(0, 1).equals(" ")) {
             ingredient = ingredient.substring(1);
         }
 
-        if (ingredient.substring(ingredient.length()-1,ingredient.length()).equals(" ")) {
-            ingredient = ingredient.substring(0,ingredient.length()-1);
+        if (ingredient.substring(ingredient.length() - 1, ingredient.length()).equals(" ")) {
+            ingredient = ingredient.substring(0, ingredient.length() - 1);
         }
 
-        ingredient.replaceAll("  "," ");
+        ingredient.replaceAll("  ", " ");
 
         return ingredient;
 
@@ -114,7 +114,7 @@ public class Utilities {
      * @param ingredient An ingredient.
      * @return An ingredient cleaned to match the app's ingredient format.
      */
-    public static String cleanString (String ingredient) {
+    public static String cleanString(String ingredient) {
 
         ingredient = stripUnicode(ingredient);
 
